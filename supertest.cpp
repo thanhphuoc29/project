fea37fbadeb990eeb5e0f7fcb9b15784a2534040
thanhphuoc29 <84950188+thanhphuoc29@users.noreply.github.com>
2022-04-12 12:28:00 +0700

    Update supertest.cpp

--- supertest.cpp
+++ supertest.cpp
@@ -1,13 +1,12 @@
 #include<bits/stdc++.h>
-
+#define ll long long
 using namespace std;
 
 int a[1005][1005],V,E,u,check[1005]={0},n,kt=0;
 vector<int>point;//luu cac dinh le
 int way[1005][1005];// ma tran luu cac doan duong di, cac duong noi dinh chan gan =1, cac dinh le gan =2
 const int maxn = 100001;
-vector<pair<int, int>> adj[maxn];
-int a[1000][1000]={};
+vector<pair<int, int> > adj[maxn];
 vector<int>dl;
 vector<int>hv[maxn];
 vector<int>gt_hv;
@@ -17,103 +16,6 @@ int pre[maxn];
  
 
 /******************Nhap ma tran*********************/
-void partition(int s,int t)//phan hoach
-{
-	vector<ll> d(n + 1, INF);
-	d[s] = 0;
-	pre[s]=s; 
-	priority_queue<pair<int, int>, vector<pair<int, int>> , greater<pair<int,int>>> Q;
-	Q.push({0, s});
-	while(!Q.empty())
-	{
-		pair<int, int> top = Q.top(); Q.pop();
-		int u = top.second;
-		int kc = top.first;
-		if(kc > d[u]) continue;
-		for(auto it : adj[u])
-		{
-			int v = it.first;
-			int w = it.second;
-			if(d[v] > d[u] + w){
-				d[v] = d[u] + w;
-				Q.push({d[v], v});
-				pre[v] = u;
-			}
-		}
-	}
-	vector<int>path;
-	while(1)
-	{
-		path.push_back(t);
-		if(t==s)	break;
-		t = pre[t];
-	}
-	reverse(begin(path),end(path));
-	for(int i = 1;i < n;i++)
-	{
-		way[path[i]][path[i+1]]++;
-		way[path[i+1]][path[i]]++;
-	}
-}
-int find_way(int s,int t)
-{
-	vector<ll> d(n + 1, INF);
-	d[s] = 0;
-	pre[s]=s; 
-	priority_queue<pair<int, int>, vector<pair<int, int>> , greater<pair<int,int>>> Q;
-	Q.push({0, s});
-	while(!Q.empty())
-	{
-		pair<int, int> top = Q.top(); Q.pop();
-		int u = top.second;
-		int kc = top.first;
-		if(kc > d[u]) continue;
-		for(auto it : adj[u])
-		{
-			int v = it.first;
-			int w = it.second;
-			if(d[v] > d[u] + w){
-				d[v] = d[u] + w;
-				Q.push({d[v], v});
-				pre[v] = u;
-			}
-		}
-	}
-	return d[t];
-}
-
-void xu_ly_dinh_le()
-{
-	for(int i = 1;i <= n;i++)
-	{
-		if(adj[i].size()%2==1)	dl.push_back(i);
-	}
-	int k = 0,k1 = 0;
-	while(next_permutation(dl.begin(),dl.end()))
-	{
-		k++;
-		if(k%2==1)
-		{
-			int value = 0;
-			for(int i = 0;i < dl.size();i+=2)
-			{
-				hv[k1].push_back(dl[i]);
-				hv[k1].push_back(dl[i+1]);
-				value += dijkstra(dl[i],dl[i+1]);
-			}
-			k1++;
-			gt_hv.push_back(value);
-		}
-	}
-	int gt = gt_hv.size();
-	int a[gt];
-	copy(gt_hv.begin(),gt_hv.end(),a);
-	int tmp = min_element(a,a+gt)-a;
-	for(int i = 0;i < hv[tmp].size();i+=2)	
-	{
-		dijkstra1(hv[tmp][i],hv[tmp][i+1]);
-	}
-}
 void input()
 {
 	for(int i = 1;i <= V;i++)
@@ -134,13 +36,11 @@ void input()
 			}
 		}	
 	}
-	xu_ly_dinh_le();
-	}
 }
-void dfs(int u){
+void dfs(int u){//duyet cac dinh bang dfs
 	check[u]=1;
 	for(int i=1;i<=V;i++){
-		if(a[u][i]==1 && check[i]==0){
+		if(a[u][i]>0 && check[i]==0){
 			dfs(i);
 		}
 	}
@@ -160,7 +60,7 @@ bool check_euler(){
 		for(int i=1;i<=V;i++){
 			d=0;
 			for(int j=1;j<=V;j++){
-				if(a[i][j]==1) d++;
+				if(a[i][j]>0) d++;
 			}
 			if(d%2 != 0) return false;//kien tra dinh le
 		}
@@ -172,25 +72,10 @@ bool check_euler(){
 int ke(int u){
 	int kt=0;
 	for(int i=1;i<=V;i++){
-		if(a[u][i]==1) return i;
+		if(a[u][i]>0) return i;
 	}
 	return 0;
 }
-/****************************Tim dinh le*************************/
-void find_odd_point(){
-	int d=0;
-	for(int i=1;i<=V;i++){
-		d=0;
-		for(int j=1;j<=V;j++){
-			if(a[i][j]>0) d++;
-		}
-		if(d%2 != 0) point.push_back(i);//luu cac dinh le
-	}
-}
-/*********************Tim duong di giua hai dinh le****************************/
-
-/************************Them canh vao do thi************************/
-
 /**************************Tim chu trinh euler******************************/
 void find_euler(int u){
 	stack<int>s;
@@ -202,7 +87,7 @@ void find_euler(int u){
 			int t=ke(d);
 			s.push(t);
 			way[d][t]--;way[t][d]--;//xoa nhung duong da di qua
-			if(odd[d][t]==0 && odd[t][d]==0){//neu khong con duong di => loai canh khoi do thi
+			if(way[d][t]==0 && way[t][d]==0){//neu khong con duong di => loai canh khoi do thi
 				a[d][t]=0;
 				a[t][d]=0;
 			}
@@ -214,6 +99,107 @@ void find_euler(int u){
 	}
 	for(int i=CE.size()-1;i>=0;i--) cout<<CE[i]<<" ";
 }
+void trace(int s,int t)//truy vet
+{
+	vector<ll> d(V + 1, INF);
+	d[s] = 0;
+	pre[s]=s; 
+	priority_queue<pair<int, int>, vector<pair<int, int> > , greater<pair<int,int> > > Q;
+	Q.push({0, s});
+	while(!Q.empty())
+	{
+		pair<int, int> top = Q.top(); Q.pop();
+		int u = top.second;
+		int kc = top.first;
+		if(kc > d[u]) continue;
+		for(int i = 0;i < adj[u].size();i++)
+		{
+			pair<int,int>it = adj[u][i];
+			int v = it.first;
+			int w = it.second;
+			if(d[v] > d[u] + w){
+				d[v] = d[u] + w;
+				Q.push({d[v], v});
+				pre[v] = u;
+			}
+		}
+	}
+	vector<int>path;
+	while(1)
+	{
+		path.push_back(t);
+		if(t==s)	break;
+		t = pre[t];
+	}
+	reverse(path.begin(),path.end());
+	for(int i = 0;i < path.size()-1;i++)
+	{
+		way[path[i]][path[i+1]]++;
+		way[path[i+1]][path[i]]++;
+	}
+}
+int find_way(int s,int t)
+{
+	vector<ll> d(V + 1, INF);
+	d[s] = 0;
+	pre[s]=s; 
+	priority_queue<pair<int, int>, vector<pair<int, int> > , greater<pair<int,int> > > Q;
+	Q.push({0, s});
+	while(!Q.empty())
+	{
+		pair<int, int> top = Q.top(); Q.pop();
+		int u = top.second;
+		int kc = top.first;
+		if(kc > d[u]) continue;
+		for(int i = 0;i < adj[u].size();i++)
+		{
+			pair<int,int>it = adj[u][i];
+			int v = it.first;
+			int w = it.second;
+			if(d[v] > d[u] + w){
+				d[v] = d[u] + w;
+				Q.push({d[v], v});
+				pre[v] = u;
+			}
+		}
+	}
+	return d[t];
+}
+
+void creat_graph()
+{
+	for(int i = 1;i <= V;i++)
+	{
+		if(adj[i].size()%2==1)	dl.push_back(i);
+	}
+	int k = 0,k1 = 0;
+	while(next_permutation(dl.begin(),dl.end()))
+	{
+		k++;
+		if(k%2==1)
+		{
+			int value = 0;
+			for(int i = 0;i < dl.size();i+=2)
+			{
+				hv[k1].push_back(dl[i]);
+				hv[k1].push_back(dl[i+1]);
+				value += find_way(dl[i],dl[i+1]);
+			}
+			k1++;
+			gt_hv.push_back(value);
+		}
+	}
+	int gt = gt_hv.size();
+	int a[gt];
+	copy(gt_hv.begin(),gt_hv.end(),a);
+	int tmp = min_element(a,a+gt)-a;
+	for(int i = 0;i < hv[tmp].size();i+=2)	
+	{
+		trace(hv[tmp][i],hv[tmp][i+1]);
+	}
+}
+
+
 int main(){
 	cout<< "---------- Chu trinh euler va bai toan nguoi dua thu ------------"<<endl;
 	cout<<"Moi ban chon che do nhap: (1/2)"<<endl;
@@ -248,11 +234,9 @@ int main(){
 		}
 		else{
 			cout<<"Khong phai do thi euler !"<<endl;
-			cout<<"Khoi tao do thi euler moi\n Hanh trinh toi uu cua nguoi dua thu la: ";
-			
-			partition();//phan hoach
-			add_path();
-			find_euler();
+			cout<<"Khoi tao do thi euler moi...\nHanh trinh toi uu cua nguoi dua thu la: ";
+			creat_graph();
+			find_euler(u);
 		}
 	}
 	else cout<<"Do thi khong lien thong!";
