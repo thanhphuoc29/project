553c47f02ea984bc7ffd3d7472c257e7d3d708c8
KatFindAnswer <89763547+KatFindAnswer@users.noreply.github.com>
2022-04-14 18:51:45 +0700

    Update supertest.cpp

--- supertest.cpp
+++ supertest.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int a[1005][1005],V,E,u,check[1005]={0},n,kt=0;
-int way[1005][1005];// ma tran luu cac doan duong di, cac duong noi dinh chan gan =1, cac dinh le gan =2
+int way[1005][1005];  				// ma tran luu cac doan duong di, cac duong noi dinh chan gan =1, cac dinh le gan =2
 const int maxn = 100001;
-vector<pair<int, int> > adj[maxn];//luu cac dinh ke va trong so vd dinh 1 ke voi 2,3 => adj[1]={2,3}
-vector<int>dl;//luu cac dinh le
-vector<int>hv[maxn];//luu hoan vi
-vector<int>gt_hv;//luu trong so cua cac hoan vi
+vector<pair<int, int> > adj[maxn];		//luu cac dinh ke va trong so vd dinh 1 ke voi 2,3 => adj[1]={2,3}
+vector<int>dl;					//luu cac dinh le
+vector<int>hv[maxn];				//luu hoan vi
+vector<int>gt_hv;				//luu trong so cua cac hoan vi
 
 const int INF = 1e9;
-int pre[maxn];//luu dinh truoc cua s
+int pre[maxn];					//luu dinh truoc cua s
  
 
 /******************Nhap ma tran*********************/
@@ -23,36 +23,32 @@ void input()
 		{
 			cin >> a[i][j];
 			way[i][j]=1;
-			if(a[i][j] >0) adj[i].push_back({j, a[i][j]});
+			if(a[i][j] >0) adj[i].push_back({j, a[i][j]}); 			//  ma trận kề 
 		}
 	}
 }
-void dfs(int u){//duyet cac dinh bang dfs
+void dfs(int u)										//duyet cac dinh bang dfs
+{
 	check[u]=1;
-	for(int i=1;i<=V;i++){
-		if(a[u][i]>0 && check[i]==0){
-			dfs(i);
-		}
+	for(int i=1;i<=V;i++)
+	{
+		if(a[u][i]>0 && check[i]==0)	dfs(i);
 	}
 }
 /****************Kiem tra lien thong**************************/
 bool check_connected(){
 	dfs(u);
 	for(int i=1;i<=V;i++){
-		if(check[i]==0) return false;//neu ton tai dinh chua dc xet se tra ve false
+		if(check[i]==0) return false;						//neu ton tai dinh chua dc xet se tra ve false
 	}
 	return true;//cac dinh da dc xet
 }
 /**********************Kiem tra euler**************************/
 bool check_euler(){
-	if(check_connected()){
-		int d=0;
-		for(int i=1;i<=V;i++){
-			d=0;
-			for(int j=1;j<=V;j++){
-				if(a[i][j]>0) d++;
-			}
-			if(d%2 != 0) return false;//kien tra dinh le
+	if(check_connected()){	
+		for(int i=1;i<=V;i++)
+		{
+		 	if(adj[i].size()%2==1)	return false;				//kien tra dinh le
 		}
 		return true;
 	}
@@ -62,9 +58,9 @@ bool check_euler(){
 int ke(int u){
 	int kt=0;
 	for(int i=1;i<=V;i++){
-		if(a[u][i]>0) return i;//tra ve dinh ke cua u
+		if(a[u][i]>0) return i;							//tra ve dinh ke cua u
 	}
-	return 0;//khong co se tra ve 0
+	return 0;									//khong co se tra ve 0
 }
 /**************************Tim chu trinh euler******************************/
 void find_euler(int u){
@@ -76,8 +72,8 @@ void find_euler(int u){
 		if(ke(d) != 0){
 			int t=ke(d);
 			s.push(t);
-			way[d][t]--;way[t][d]--;//xoa nhung duong da di qua
-			if(way[d][t]==0 && way[t][d]==0){//neu khong con duong di => loai canh khoi do thi
+			way[d][t]--;way[t][d]--;					//xoa nhung duong da di qua
+			if(way[d][t]==0 && way[t][d]==0){				//neu khong con duong di => loai canh khoi do thi
 				a[d][t]=0;
 				a[t][d]=0;
 			}
@@ -89,25 +85,25 @@ void find_euler(int u){
 	}
 	for(int i=CE.size()-1;i>=0;i--) cout<<CE[i]<<" ";
 }
-void trace(int s,int t)//truy vet + phan hoach
+void trace(int s,int t)									//truy vet + phan hoach
 {
 	vector<ll> d(V + 1, INF);
 	d[s] = 0;//gan nhan d[s]=0
 	pre[s]=s; 
-	priority_queue<pair<int, int>, vector<pair<int, int> > , greater<pair<int,int> > > Q;//dung hang doi uu tien (phan tu top se nho nhat)
+	priority_queue<pair<int, int>, vector<pair<int, int> > , greater<pair<int,int> > > Q;		//dung hang doi uu tien (phan tu top se nho nhat)
 	Q.push({0, s});
 	while(!Q.empty())
 	{
 		pair<int, int> top = Q.top(); Q.pop();/
-		int u = top.second;//s
-		int kc = top.first;//d[s]
-		if(kc > d[u]) continue;
+		int u = top.second;									//s
+		int kc = top.first;									//d[s]
+		if(kc > d[u]) continue;									// tránh trong stack có trường hợp (1,2),(2,2) sẽ xét cả (2,2)
 		for(int i = 0;i < adj[u].size();i++)
 		{
 			pair<int,int>it = adj[u][i];
-			int v = it.first;// lay ra dinh ke voi u
-			int w = it.second;//lay ra trong so (khoang cach tu u->i
-			if(d[v] > d[u] + w){
+			int v = it.first;								// lay ra dinh ke voi u
+			int w = it.second;								//lay ra trong so (khoang cach tu u->i)
+			if(d[v] > d[u] + w){								//xét cách đi đến đỉnh v theo cách mới( nếu khoảng cách  3-4 > 3 -4 -2 thì đi qua 3-4-2)
 				d[v] = d[u] + w;
 				Q.push({d[v], v});
 				pre[v] = u;
@@ -125,11 +121,11 @@ void trace(int s,int t)//truy vet + phan hoach
 	reverse(path.begin(),path.end());
 	for(int i = 0;i < path.size()-1;i++)
 	{
-		way[path[i]][path[i+1]]++;//them duong di tu u->v vao do thi
-		way[path[i+1]][path[i]]++;//them duong di tu v->u vao do thi
+		way[path[i]][path[i+1]]++;								//them duong di tu u->v vao do thi
+		way[path[i+1]][path[i]]++;								//them duong di tu v->u vao do thi
 	}
 }
-int find_way(int s,int t)//dung thuat toan diijectra
+int find_way(int s,int t)			//dung thuat toan dijkstra
 {
 	vector<ll> d(V + 1, INF);
 	d[s] = 0;
@@ -154,7 +150,7 @@ int find_way(int s,int t)//dung thuat toan diijectra
 			}
 		}
 	}
-	return d[t];
+	return d[t];		trả về tổng đường đi ngắn nhất từ s đến t
 }
 
 void creat_graph()
